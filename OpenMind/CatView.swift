fb918a937196b091751628e3f161a6010705e402
Vladimir Fibe <motiw@icloud.com>
2023-07-07 05:10:11 +0600

    Colors

--- OpenMind/CatView.swift
+++ OpenMind/CatView.swift
@@ -1,20 +1,35 @@
 import SwiftUI
 
 struct CatView: View {
+    var gradient: Gradient {
+        let stops: [Gradient.Stop] = [
+            .init(color: .omViolet, location: 0.0),
+            .init(color: .indigo, location: 0.4),
+            .init(color: .mint, location: 0.45)
+        ]
+        return Gradient(stops: stops)
+    }
     var body: some View {
         ZStack {
-            Color.teal
+            Color(.tertiarySystemBackground)
                 .ignoresSafeArea()
-            Image(.ozma)
-                .resizable()
-                .scaledToFit()
-                .clipShape(Circle())
-                .overlay {
-                    Circle()
-                        .stroke(lineWidth: 8)
-                        .foregroundStyle(.white)
-                }
-                .frame(width: 250)
+            VStack {
+                Image(.ozma)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250)
+                    .clipShape(Circle())
+                    .overlay {
+                        LinearGradient(gradient: gradient, startPoint: .bottom, endPoint: .top)
+                            .clipShape(
+                                Circle().stroke(lineWidth: 16)
+                            )
+                    }
+                Text("Ozma")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.secondary)
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+#  OpenMind
+
+[Color](https://developer.apple.com/design/human-interface-guidelines/color)
+
+[Drawing in iOS with SwiftUI](https://www.raywenderlich.com/30399648-drawing-in-ios-with-swiftui)
+
