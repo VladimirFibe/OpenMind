257e9e8dfd6e8928082fdb7c1b96ec64f79bce35
Vladimir Fibe <motiw@icloud.com>
2023-07-10 09:07:04 +0600

    fix bug with animation

--- OpenMind/CellView.swift
+++ OpenMind/CellView.swift
@@ -8,13 +8,13 @@ struct CellView: View {
         cell == cellStore.selectedCell
     }
     var body: some View {
-        let basicStyle = StrokeStyle(lineWidth: 5, lineJoin: .round)
-        let selectedStyle = StrokeStyle(lineWidth: 7, lineCap: .round, lineJoin: .round, dash: [50, 15, 30, 15, 15, 15, 5, 10, 5, 15], dashPhase: 0)
+        
         ZStack {
             cell.shape?.shape
                 .foregroundStyle(.white)
-            cell.shape?.shape
-                .stroke(cell.color, style: isSelected ? selectedStyle : basicStyle)
+            TimelineView(.animation(minimumInterval: 0.2)) { context in
+                StrokeView(cell: cell, isSelected: isSelected, date: context.date)
+            }
             TextField("Enter cell text", text: $text)
                 .padding()
                 .multilineTextAlignment(.center)
@@ -23,6 +23,26 @@ struct CellView: View {
         .offset(cell.offset)
         .onAppear { text = cell.text}
         .onTapGesture { cellStore.selectedCell = cell }
+    }
+}
+
+extension CellView {
+    
+    struct StrokeView: View {
+        let cell: Cell
+        let isSelected: Bool
+        let date: Date
+        @State var dashPhase = 0.0
+        var body: some View {
+            let basicStyle = StrokeStyle(lineWidth: 5, lineJoin: .round)
+            let selectedStyle = StrokeStyle(lineWidth: 7, lineCap: .round, lineJoin: .round, dash: [50, 15, 30, 15, 15, 15, 5, 10, 5, 15], dashPhase: dashPhase)
+            cell.shape?.shape
+                .stroke(cell.color.opacity(isSelected ? 0.8 : 1),
+                        style: isSelected ? selectedStyle : basicStyle)
+                .onChange(of: date, { oldValue, newValue in
+                    dashPhase += 8
+                })
+        }
     }
 }
 
